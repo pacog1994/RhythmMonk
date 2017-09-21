eafe8395fc6133dd5e4d399d4028010a28e53006
Paco <pacog1994@gmail.com>
2017-09-20 17:14:01 -0700

    Assets 50% upload

---
+++ Assets/Editor/ResetGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ResetGame : EditorWindow {
+
+    [MenuItem("Edit/Reset Playerprefs")]
+
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
---
+++ Assets/Prefabs/Particle Effects/ParticleOneShot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleOneShot : MonoBehaviour {
+
+    private ParticleSystem ps;
+
+    // Initialization
+    public void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    public void Update()
+    {
+        if (ps)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
